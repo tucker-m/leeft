1b046d524d273c72d59c09c6fbf43a824e27edab
= <=>
2018-01-01 18:05:18 -0700

    Added a method to fetch an array of observables from the database. Using this method to fill the allWorkouts variable. Since they're observables, you can change the name of a workout on the workout list page now.

--- src/typescript/workoutList.ts
+++ src/typescript/workoutList.ts
@@ -5,7 +5,7 @@ import {IObservableObject} from 'mobx'
 import db from './db'
 
 interface WorkoutListAttrs {
-    allWorkouts: Array<Workout & Saveable>,
+    allWorkouts: Array<Workout & Saveable & IObservableObject>,
     allExercises: Array<Exercise>,
     saveWorkout: (w: Workout & IObservableObject, i: number) => void,
 };
@@ -14,7 +14,41 @@ interface WorkoutListVnode {
     attrs: WorkoutListAttrs
 };
 
-let WorkoutListComponent = function(vnode: WorkoutListVnode) {
+interface WorkoutTitleVnode {
+    attrs: {
+        workout: Workout & Saveable & IObservableObject,
+        beingEdited: boolean,
+    }
+}
+
+const WorkoutTitleComponent = function(vnode: WorkoutTitleVnode) {
+    let beingEdited = vnode.attrs.beingEdited
+    return {
+        view: function(vnode: WorkoutTitleVnode) {
+            let workout = vnode.attrs.workout
+            if (vnode.attrs.beingEdited) {
+                return m('div.input-group', [
+                    m('input[type=text].input-group-field', {
+                        value: workout.name,
+                        placeholder: 'Enter a name for this workout',
+                        onchange: m.withAttr('value', (value) => { workout.name = value }),
+                    }),
+                    m('div.input-group-button', m('button.button', {
+                        onclick: () => { beingEdited = false }
+                    }, 'Done'))
+                ])
+            }
+            else {
+                return m('a', {
+                    href: '/workouts/' + workout._id,
+                    oncreate: m.route.link,
+                }, workout.name)
+            }
+        }
+    }
+}
+
+const WorkoutListComponent = function(vnode: WorkoutListVnode) {
     return {
         view: function(vnode: WorkoutListVnode) {
             let elements: Array<m.Vnode<{}, {}>> = [];
@@ -23,17 +57,14 @@ let WorkoutListComponent = function(vnode: WorkoutListVnode) {
             }
             else {
                 elements.push(m('ul', vnode.attrs.allWorkouts.map((workout) => {
-                    return m('li', m('a', {
-                        href: '/workouts/' + workout._id,
-                        oncreate: m.route.link,
-                    }, workout.name))
+                    return m('li', m(WorkoutTitleComponent, {workout: workout, beingEdited: (workout.name == '')}))
                 })));
             }
             elements.push(m('button.button', {
                 onclick: preventDefault(() => {
                     let newWorkout: Workout & Saveable & IObservableObject = db.createSaveableRecord({
                         _id: 'workout_' + Date.now(), // TODO: add a random number here
-                        name: 'New Workout',
+                        name: '',
                         prescriptions: []
                     });
                     const newIndex = vnode.attrs.allWorkouts.length;

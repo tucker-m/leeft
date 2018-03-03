2ba1ad5cd10b539eac2577504c2845748c199d72
= <=>
2018-03-03 16:05:36 -0700

    Removed the edit menu from the view workout page. Changes are made from the "edit" button on top now. Workouts are no longer deleteable -- I'll have to add this functionality back in later.

--- src/typescript/workouts/viewWorkout.ts
+++ src/typescript/workouts/viewWorkout.ts
@@ -1,7 +1,6 @@
 import * as m from 'mithril';
 import db from '../helpers/db';
 import {Saveable, Workout} from '../types/exercise';
-import ViewWorkoutMenu from '../workouts/viewWorkoutMenu';
 import ViewWorkoutRow from '../workouts/viewWorkoutRow';
 import WorkoutLogs from '../workouts/workoutLogs';
 import {observable, IObservableObject} from 'mobx';
@@ -27,68 +26,31 @@ export default (vnode: ViewWorkoutVnode) => {
         m.redraw();
     })
 
-    let showMenu = false;
-    let showEditButtons = false;
-    let titleBeingEdited = false;
-    let pageEditable = false; // master edit toggle from top bar. Go with only this
+    let pageEditable = false
 
     return {
-        onupdate: (vnode: ViewWorkoutVnode) => {
-            if (titleBeingEdited) {
-                vnode.dom.querySelector('input.title').focus()
-            }
-        },
         view: (vnode: ViewWorkoutVnode) => {
             return m('div', [
                 TopBar({
                     editToggleFunc: (value) => {pageEditable = value}
                 }),
                 m('div.grid-x.grid-margin-x.align-middle', [
-                    titleBeingEdited
-                        ? m('input[type=text].cell.auto.title', {
-                            value: workout.name,
-                            onchange: m.withAttr('value', (value) => {
-                                workout.name = value;
-                            }),
-                            placeholder: "New Workout Name...",
-                        })
-                    : EditableH1({
+                    EditableH1({
                         name: workout.name,
                         updateFunc: (newName: string) => { workout.name = newName },
                         showEditButton: pageEditable,
                     }),
-                    showEditButtons || titleBeingEdited
-                        ? m('button.button.secondary', {
-                            onclick: () => {
-                                showEditButtons = false;
-                                titleBeingEdited = false;
-                            },
-                        }, 'Done')
-                    : m('button.button.dropdown.secondary.hollow.cell.shrink', {
-                        onclick: () => { showMenu = !showMenu; }
-                    }, 'Edit'),
                 ]),
-                showMenu ? m(ViewWorkoutMenu, {
-                    workout: workout,
-                    showEditButtons: () => {
-                        showEditButtons = true;
-                        showMenu = false;
-                    },
-                    editNameFunction: () => {
-                        showMenu = false;
-                        titleBeingEdited = true;
-                    }
-                }) : null,
                 m('table', [
                     m('thead', m('tr', [
                         m('td', 'Exercise'),
                         m('td', 'Amount'),
-                        showEditButtons ? m('td') : null,
+                        pageEditable ? m('td') : null,
                     ])),
                     m('tbody', workout.prescriptions.map((prescription, index) => {
                         return m(ViewWorkoutRow, {
                             prescription,
-                            showEditButtons,
+                            showEditButtons: pageEditable,
                             deleteFunction: () => {
                                 workout.prescriptions.splice(index, 1);
                             },

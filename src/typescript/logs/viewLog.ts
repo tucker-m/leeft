bb767f53bfd81392c98379914a594aa38d9d5d65
= <=>
2018-03-25 14:04:52 -0600

    Using the page object on the view log page

--- src/typescript/logs/viewLog.ts
+++ src/typescript/logs/viewLog.ts
@@ -3,6 +3,7 @@ import {WorkoutLog} from '../types/exercise'
 import db from '../helpers/db'
 import utils from '../helpers/utils'
 import preventDefault from '../helpers/preventDefaultFunction'
+import Page from '../ui/page'
 
 interface ViewLogAttrs {
     id: string
@@ -31,33 +32,36 @@ export default (vnode: ViewLogVnode) => {
             if (log == null) {
                 return m('p', 'Loading')
             }
-            return m('div', [
-                m('h1', log.workout.name == '' ? 'Untitled' : log.workout.name),
-                m('h4', utils.formatDate(log.date)),
-                m('a', {
-                    onclick: preventDefault(() => {
-                        log._deleted = true
-                        window.location.href = `#!/workouts/${log.workout._id}`
-                    })
-                }, 'Delete this log'),
-                m('table', [
-                    m('thead', [
-                        m('tr', [
-                            m('td', 'Exercise'),
-                            m('td', 'Reps'),
-                            m('td', 'Amount')
-                        ])
+            return Page({
+                topBarButtons: [],
+                contents: [
+                    m('h1', log.workout.name == '' ? 'Untitled' : log.workout.name),
+                    m('h4', utils.formatDate(log.date)),
+                    m('a', {
+                        onclick: preventDefault(() => {
+                            log._deleted = true
+                            window.location.href = `#!/workouts/${log.workout._id}`
+                        })
+                    }, 'Delete this log'),
+                    m('table', [
+                        m('thead', [
+                            m('tr', [
+                                m('td', 'Exercise'),
+                                m('td', 'Reps'),
+                                m('td', 'Amount')
+                            ])
+                        ]),
+                        m('tbody', log.sets.map((set) => {
+                            return m('tr', [
+                                m('td', set.exercise.name),
+                                m('td', set.reps),
+                                m('td', set.amount)
+                            ])
+                        }))
                     ]),
-                    m('tbody', log.sets.map((set) => {
-                        return m('tr', [
-                            m('td', set.exercise.name),
-                            m('td', set.reps),
-                            m('td', set.amount)
-                        ])
-                    }))
-                ]),
-                m('p', log.comments),
-            ])
+                    m('p', log.comments),
+                ]
+            })
         }
     }
 }

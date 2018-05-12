47102af178974e6c21e733ff3956ba7943a208b4
= <=>
2018-05-12 15:33:43 -0600

    Change wording

--- src/typescript/workouts/workoutContents.ts
+++ src/typescript/workouts/workoutContents.ts
@@ -21,6 +21,12 @@ const WorkoutContent = (vnode: ContentVnode) => {
             const pageEditable = vnode.attrs.pageEditable
             const classes = vnode.attrs.css
             return [
+                vnode.attrs.program
+                    ? m('a', {
+                        href: `/programs/${vnode.attrs.program._id}`,
+                        oncreate: m.route.link,
+                    }, `Back to ${vnode.attrs.program.name}`)
+                : null,
                 EditableH1({
                     name: workout.name,
                     placeholder: 'Untitled Workout',
@@ -28,12 +34,6 @@ const WorkoutContent = (vnode: ContentVnode) => {
                     showEditButton: pageEditable,
                     css: classes,
                 }),
-                vnode.attrs.program
-                    ? m('a', {
-                        href: `/programs/${vnode.attrs.program._id}`,
-                        oncreate: m.route.link,
-                    }, `Part of ${vnode.attrs.program.name}`)
-                    : null,
                 WorkoutTable({
                     prescriptions: workout.prescriptions,
                     showEditButtons: pageEditable,

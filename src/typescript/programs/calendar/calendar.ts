47f70112def288c82cdd2b7b4a614c3506996c2e
= <=>
2019-01-20 16:19:45 -0700

    Created a component for calendar items

--- src/typescript/programs/calendar/calendar.ts
+++ src/typescript/programs/calendar/calendar.ts
@@ -1,7 +1,7 @@
 import * as m from 'mithril'
 import {Workout, Rest, WorkoutAndLog} from '../../types/exercise'
-import EditButtons from './editButtons'
 import u from '../../helpers/utils'
+import CalendarItem from './calendarItem'
 
 interface CalendarAttrs {
     workouts: Array<WorkoutAndLog>,
@@ -41,40 +41,18 @@ const CalendarComponent = (vnode: CalendarVnode) => {
 		    const moveUp = vnode.attrs.moveUp.bind(this, index)
 		    const moveDown = vnode.attrs.moveDown.bind(this, index)
 		    const remove = vnode.attrs.remove.bind(this, index)
-		    const workoutInfo = u.getNameAndClasses(workout, css)
 
 		    return m('div', {class: css.calendarSquare}, [
 			m('div', {class: css.numberColumn}, (index+1).toString()),
-			m('div', {class: css.descriptionColumn}, [
-			    m('div',
-			      m('a', {
-				  href: `${vnode.attrs.programUrl}/workouts/${index}`,
-				  oncreate: m.route.link,
-				  class: u.c(css.workoutTitle, workoutInfo.classes)
-			      },
-				workout.tag == 'workout'
-				? workoutInfo.name
-				: 'Rest Day')),
-			    m('div', {class: css.workoutDescription}, m('div', [
-				m('p', workout.tag == 'workout' ? u.getWorkoutExercisesElement(workout, css) : ''),
-
-				m('p', {class: css.lastWorkout},
-				  vnode.attrs.beingEdited
-				  ? EditButtons({
-				      moveUp: (index > 0) ? moveUp : null,
-				      moveDown: (index < workouts.length - 1) ? moveDown : null,
-				      remove,
-				      css,
-				  })
-				  : ['Last Workout: ',
-				     m('a', {
-					 class: css.dateLink,
-					 href: `/logs/${workout.lastLog.id}`,
-					 oncreate: m.route.link,
-				     }, u.formatDate(workout.lastLog.date))],
-				 ),
-			    ]))
-			]),
+			m('div', {class: css.descriptionColumn}, CalendarItem({
+			    workout,
+			    beingEdited: vnode.attrs.beingEdited,
+			    workoutUrl: `${vnode.attrs.programUrl}/workouts/${index}`,
+			    moveUp: (index > 0) ? moveUp : null,
+			    moveDown: (index < workouts.length - 1) ? moveDown : null,
+			    remove,
+			    css,
+			}))
 		    ])
 		})
 	    ])
---
+++ src/typescript/programs/calendar/calendarItem.ts
@@ -0,0 +1,62 @@
+import * as m from 'mithril'
+import u from '../../helpers/utils'
+import {WorkoutAndLog} from '../../types/exercise'
+import EditButtons from './editButtons'
+
+interface CalendarItemAttrs {
+    workout: WorkoutAndLog,
+    beingEdited: boolean,
+    workoutUrl: string,
+    moveUp?: () => void,
+    moveDown?: () => void,
+    remove: () => void,
+    css: any,
+}
+interface CalendarItemVnode {
+    attrs: CalendarItemAttrs
+}
+
+const CalendarItem = (vnode: CalendarItemVnode) => {
+    const css = vnode.attrs.css
+    
+    return {
+	view: (vnode: CalendarItemVnode) => {
+	    const workout = vnode.attrs.workout
+	    const workoutInfo = u.getNameAndClasses(workout, css)
+
+	    return [
+		m('div',
+		  m('a', {
+		      href: vnode.attrs.workoutUrl,
+		      oncreate: m.route.link,
+		      class: u.c(css.workoutTitle, workoutInfo.classes)
+		  },
+		    workout.tag == 'workout'
+		    ? workoutInfo.name
+		    : 'Rest Day')),
+		m('div', {class: css.workoutDescription}, m('div', [
+		    m('p', workout.tag == 'workout' ? u.getWorkoutExercisesElement(workout, css) : ''),
+		    m('p', {class: css.lastWorkout},
+		      vnode.attrs.beingEdited
+		      ? EditButtons({
+			  moveUp: vnode.attrs.moveUp,
+			  moveDown: vnode.attrs.moveDown,
+			  remove: vnode.attrs.remove,
+			  css,
+		      })
+		      : ['Last Workout: ',
+			 m('a', {
+			     class: css.dateLink,
+			     href: `/logs/${workout.lastLog.id}`,
+			     oncreate: m.route.link,
+			 }, u.formatDate(workout.lastLog.date))],
+		     ),
+		]))
+	    ]
+	}
+    }
+}
+
+export default (attrs: CalendarItemAttrs) => {
+    return m(CalendarItem, attrs)
+}

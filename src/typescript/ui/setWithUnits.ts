62a4cd296f7b8faeb5c413e23a92315ec64080db
= <=>
2019-09-15 16:01:00 -0600

    Move the set/unit pill buttons to a component

---
+++ src/typescript/ui/setWithUnits.ts
@@ -0,0 +1,100 @@
+import * as m from 'mithril'
+import {Set, SetGroup} from '../types/exercise'
+import AddSetOverlay from '../workouts/overlays/exercise/set'
+
+interface SetAttrs {
+    setGroup: SetGroup,
+    showEditButtons: boolean,
+    setOverlay: (overlay: any, attrs: any) => void,
+    css: any,
+}
+interface SetVnode {
+    attrs: SetAttrs
+}
+
+export default {
+    view: (vnode: SetVnode) => {
+	const {setGroup, css, showEditButtons} = vnode.attrs
+
+	return m('ul', {class: css.setUl}, setGroup.sets.map((set, index) => {
+	    let unitParts: any[] = []
+	    if (set.reps) {
+		if (set.reps.prescribed) {
+		    unitParts.push(m('span', {class: css.repPill}, `${set.reps.prescribed} reps`))
+		}
+		else {
+		    unitParts.push(m('span', {class: css.repBlank}, '____ reps'))
+		}
+	    }
+	    if (set.weight) {
+		if (set.weight.prescribed) {
+		    unitParts.push(m('span', {class: css.weightPill}, `${set.weight.prescribed} pounds`))
+		}
+		else {
+		    unitParts.push(m('span', {class: css.weightBlank}, '____ pounds'))
+		}
+	    }
+	    if (set.time) {
+		if (set.time.prescribed) {
+		    unitParts.push(m('span', {class: css.timePill}, `${set.time.prescribed} seconds`))
+		}
+		else {
+		    unitParts.push(m('span', {class: css.timeBlank}, '____ seconds'))
+		}
+	    }
+	    return m('li', {class: css.setLi}, [
+		showEditButtons
+		    ? m('div', {class: css.upDownBtnGroup}, [
+			(index > 0)
+			    ? m('button', {
+				class: css.upBtnSmall,
+				onclick: () => {
+				    setGroup.sets.splice(index, 1)
+				    setGroup.sets.splice(index - 1, 0, set)
+				}
+			    })
+			    : null,
+			(index < setGroup.sets.length - 1)
+			    ? m('button', {
+				class: css.downBtnSmall,
+				onclick: () => {
+				    setGroup.sets.splice(index, 1)
+				    setGroup.sets.splice(index + 1, 0, set)
+				}
+			    })
+			    : null,
+		    ]) : null,
+		m('span', {class: css.setNumber}, index+1),
+		(unitParts.length > 0)
+		    ? unitParts
+		    : m('span', 'No goals or units given for this set.'),
+		showEditButtons
+		    ? [
+			m('button', {
+			    class: `${css.hollowEditButton} ${css.small}`,
+			    onclick: () => {
+				vnode.attrs.setOverlay(AddSetOverlay, {
+				    title: setGroup.exerciseName,
+				    index,
+				    set,
+				    addSet: (changedSet: Set) => {
+					setGroup.sets.splice(index, 1, changedSet)
+				    },
+				    hideOverlay: () => {
+					vnode.attrs.setOverlay({component: null, title: ''}, {})
+				    },
+				    css,
+				})
+			    }
+			}, 'Edit'),
+			m('button', {
+			    class: `${css.hollowDangerButton} ${css.small}`,
+			    onclick: () => {
+				setGroup.sets.splice(index, 1)
+			    }
+			}, 'Delete'),
+		    ] : null,
+	    ])
+	}))
+    }
+}
--- src/typescript/ui/workoutTable.ts
+++ src/typescript/ui/workoutTable.ts
@@ -4,6 +4,7 @@ import {Set, SetGroup} from '../types/exercise'
 import utils from '../helpers/utils'
 import ExerciseOverlay from '../workouts/overlays/exercise/exercise'
 import AddSetOverlay from '../workouts/overlays/exercise/set'
+import SetWithUnits from '../ui/setWithUnits'
 
 interface TableAttributes {
     prescriptions: SetGroup[],
@@ -84,88 +85,12 @@ const TableComponent = (vnode: TableVnode) => {
 				    m('div', [
 					(setGroup.sets.length == 0)
 					    ? m('p', 'No sets specified.')
-					    : [
-						m('ul', {class: css.setUl}, setGroup.sets.map((set, index) => {
-						    let unitParts: any[] = []
-						    if (set.reps) {
-							if (set.reps.prescribed) {
-							    unitParts.push(m('span', {class: css.repPill}, `${set.reps.prescribed} reps`))
-							}
-							else {
-							    unitParts.push(m('span', {class: css.repBlank}, '____ reps'))
-							}
-						    }
-						    if (set.weight) {
-							if (set.weight.prescribed) {
-							    unitParts.push(m('span', {class: css.weightPill}, `${set.weight.prescribed} pounds`))
-							}
-							else {
-							    unitParts.push(m('span', {class: css.weightBlank}, '____ pounds'))
-							}
-						    }
-						    if (set.time) {
-							if (set.time.prescribed) {
-							    unitParts.push(m('span', {class: css.timePill}, `${set.time.prescribed} seconds`))
-							}
-							else {
-							    unitParts.push(m('span', {class: css.timeBlank}, '____ seconds'))
-							}
-						    }
-						    return m('li', {class: css.setLi}, [
-							showEditButtons
-							    ? m('div', {class: css.upDownBtnGroup}, [
-								(index > 0)
-								    ? m('button', {
-									class: css.upBtnSmall,
-									onclick: () => {
-									    setGroup.sets.splice(index, 1)
-									    setGroup.sets.splice(index - 1, 0, set)
-									}
-								    })
-								    : null,
-								(index < setGroup.sets.length - 1)
-								    ? m('button', {
-									class: css.downBtnSmall,
-									onclick: () => {
-									    setGroup.sets.splice(index, 1)
-									    setGroup.sets.splice(index + 1, 0, set)
-									}
-								    })
-								    : null,
-							    ]) : null,
-							m('span', {class: css.setNumber}, index+1),
-							(unitParts.length > 0)
-							    ? unitParts
-							    : m('span', 'No goals or units given for this set.'),
-							showEditButtons
-							    ? [
-								m('button', {
-								    class: `${css.hollowEditButton} ${css.small}`,
-								    onclick: () => {
-									vnode.attrs.setOverlay(AddSetOverlay, {
-									    title: setGroup.exerciseName,
-									    index,
-									    set,
-									    addSet: (changedSet: Set) => {
-										setGroup.sets.splice(index, 1, changedSet)
-									    },
-									    hideOverlay: () => {
-										vnode.attrs.setOverlay({component: null, title: ''}, {})
-									    },
-									    css,
-									})
-								    }
-								}, 'Edit'),
-								m('button', {
-								    class: `${css.hollowDangerButton} ${css.small}`,
-								    onclick: () => {
-									setGroup.sets.splice(index, 1)
-								    }
-								}, 'Delete'),
-							    ] : null,
-						    ])
-						}))
-					    ],
+					    : m(SetWithUnits, {
+						setGroup,
+						showEditButtons,
+						setOverlay: vnode.attrs.setOverlay,
+						css,
+					    }),
 					(vnode.attrs.showEditButtons)
 					    ? m('button', {
 						class: css.hollowButton,

e33423091cb5473a33fc2b57b5369f64b06a1e7d
= <=>
2019-09-28 16:10:26 -0600

    Show the completed sets if the set has been done
    Also change the pill styles for prescribed reps.
    Prescribed is hollow, completed is filled in.

--- src/typescript/ui/setWithUnits.ts
+++ src/typescript/ui/setWithUnits.ts
@@ -20,8 +20,9 @@ export default {
 	const {setOverlay, setGroup, css, showEditButtons, showLogButton} = vnode.attrs
 
 	return m('ul', {class: css.setUl}, setGroup.sets.map((set, index) => {
+	    const setIsDone = (set.reps && set.reps.entered !== false) || (set.weight && set.weight.entered !== false) || (set.time && set.time.entered !== false)
 	    let unitParts: any[] = []
-	    if (set.reps) {
+	    if (set.reps && !setIsDone) {
 		if (set.reps.prescribed) {
 		    unitParts.push(m('span', {class: css.repPill}, `${set.reps.prescribed} reps`))
 		}
@@ -29,7 +30,10 @@ export default {
 		    unitParts.push(m('span', {class: css.repBlank}, '____ reps'))
 		}
 	    }
-	    if (set.weight) {
+	    else if (set.reps && set.reps.entered){
+		unitParts.push(m('span', {class: css.repDone}, `${set.reps.entered} reps`))
+	    }
+	    if (set.weight && !setIsDone) {
 		if (set.weight.prescribed) {
 		    unitParts.push(m('span', {class: css.weightPill}, `${set.weight.prescribed} pounds`))
 		}
@@ -37,13 +41,19 @@ export default {
 		    unitParts.push(m('span', {class: css.weightBlank}, '____ pounds'))
 		}
 	    }
-	    if (set.time) {
+	    else if (set.weight && set.weight.entered) {
+		unitParts.push(m('span', {class: css.weightDone}, `${set.weight.entered} pounds`))
+	    }
+	    if (set.time && !setIsDone) {
 		if (set.time.prescribed) {
 		    unitParts.push(m('span', {class: css.timePill}, `${set.time.prescribed} seconds`))
 		}
 		else {
 		    unitParts.push(m('span', {class: css.timeBlank}, '____ seconds'))
 		}
+	    }
+	    else if (set.time && set.time.entered) {
+		unitParts.push(m('span', {class: css.timeDone}, `${set.time.entered} seconds`))
 	    }
 	    return m('li', {class: css.setLi}, [
 		showEditButtons
@@ -82,7 +92,7 @@ export default {
 			}
 		    }, 'Enter log')
 		    : null,
-		(set.reps && set.reps.entered !== false) || (set.weight && set.weight.entered !== false) || (set.time && set.time.entered !== false)
+		(setIsDone)
 		    ? m('span', {class: css.setDone}, 'X')
 		    : m('span', {class: css.setNumber}, index+1),
 		(unitParts.length > 0)

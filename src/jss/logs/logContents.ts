b64f2772e1f1d49358ff181aea96c8484154a906
= <=>
2019-09-29 15:47:16 -0600

    Remove left border that grouped exercises

--- src/jss/logs/logContents.ts
+++ src/jss/logs/logContents.ts
@@ -2,8 +2,6 @@ import colors from '../variables/colors'
 
 export default {
     exerciseLogContainer: {
-	borderLeft: `.25rem solid ${colors.secondary}`,
-	paddingLeft: '1rem',
 	marginBottom: '1rem',
 	'& li:not(:last-of-type)': {
 	    marginBottom: '.75rem',
